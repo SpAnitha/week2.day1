b9110311b183a656ce3f6b0ae5aafc4698a4b266
SpAnitha <103819801+SpAnitha@users.noreply.github.com>
2022-05-16 22:16:23 +0530

    Add files via upload

---
+++ LearnString.java
@@ -0,0 +1,12 @@
+package week2.day1;
+
+public class LearnString {
+	public static void main(String[]args) {
+		String input="Testleaf is situtated in twin towers teynampet";
+		for(String value: input.split(" ")) {
+			if(value.startsWith("T")||value.startsWith("t")){
+				System.out.println(value);
+			}
+		}
+	}
+}
---
+++ LearnString2.java
@@ -0,0 +1,15 @@
+package week2.day1;
+
+public class LearnString2 {
+	public static void main(String[]args) {
+		String text="testleaf";
+		int num=0;
+		for(int i=0;i<text.length();i++) {
+			if(text.charAt(i)== 'e')
+				num++;
+		}
+
+		System.out.println("Number of e:"  +num);
+
+	}
+}
